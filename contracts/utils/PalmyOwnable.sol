ef87895d729357c74e2e0444c9a1627af7fdd6a1
hide-yoshi <hide-yoshi@users.noreply.github.com>
2023-11-03 13:53:50 +0900

    update comment

--- contracts/utils/PalmyOwnable.sol
+++ contracts/utils/PalmyOwnable.sol
@@ -1,11 +1,9 @@
-
-// contracts/TokenVesting.sol
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity 0.8.11;
-import "@openzeppelin/contracts/access/Ownable.sol";
+import '@openzeppelin/contracts/access/Ownable.sol';
 
 abstract contract PalmyOwnable is Ownable {
-    constructor(address owner) {
-        _transferOwnership(owner);
-    }
+  constructor(address owner) {
+    _transferOwnership(owner);
+  }
 }
