fb2f0d4a4711fee504d5524513412c8b0b38e87e
hide-yoshi <hide-yoshi@users.noreply.github.com>
2023-11-11 20:57:48 +0900

    update initialization

--- tasks/deployments/initialize-RewardsVault.ts
+++ tasks/deployments/initialize-RewardsVault.ts
@@ -6,52 +6,55 @@ import {
   getPalmyRewardsVault,
 } from '../../helpers/contracts-helpers';
 import { waitForTx } from '../../helpers/misc-utils';
-import { ZERO_ADDRESS, getWOASTokenPerNetwork } from '../../helpers/constants';
+import {
+  ZERO_ADDRESS,
+  getIncentivesControllerPerNetwork,
+  getWOASTokenPerNetwork,
+} from '../../helpers/constants';
 import { eEthereumNetwork } from '../../helpers/types-common';
 
 const { PalmyRewardsVault } = eContractid;
 
-task(`initialize-${PalmyRewardsVault}`, `Initialize the ${PalmyRewardsVault} proxy contract`)
-  .addParam('incentivesController', `The address of IncentivesController.`)
-  .addFlag('onlyProxy', 'Initialize only the proxy contract, not the implementation contract')
-  .setAction(async ({ incentivesController }, localBRE) => {
-    await localBRE.run('set-dre');
+task(
+  `initialize-${PalmyRewardsVault}`,
+  `Initialize the ${PalmyRewardsVault} proxy contract`
+).setAction(async ({}, localBRE) => {
+  await localBRE.run('set-dre');
 
-    if (!localBRE.network.config.chainId) {
-      throw new Error('INVALID_CHAIN_ID');
-    }
-    if (!incentivesController) {
-      throw new Error(`Missing --incentives-controller parameter`);
-    }
+  if (!localBRE.network.config.chainId) {
+    throw new Error('INVALID_CHAIN_ID');
+  }
 
-    console.log(`\n- ${PalmyRewardsVault} initialization`);
+  console.log(`\n- ${PalmyRewardsVault} initialization`);
+  const incentivesController = await getIncentivesControllerPerNetwork(
+    localBRE.network.name as eEthereumNetwork
+  );
+  const rewardsVaultImpl = await getPalmyRewardsVaultImpl();
+  const rewardsVaultProxy = await getPalmyRewardsVault();
+  const woas = await getWOASTokenPerNetwork(localBRE.network.name as eEthereumNetwork);
+  if (!rewardsVaultImpl.address || rewardsVaultImpl.address == ZERO_ADDRESS) {
+    throw new Error('Missing rewardsVaultImpl');
+  }
+  if (!rewardsVaultProxy.address || rewardsVaultImpl.address == ZERO_ADDRESS) {
+    throw new Error('Missing rewardsVaultProxy');
+  }
+  if (!woas || rewardsVaultImpl.address == ZERO_ADDRESS) {
+    throw new Error('Missing plmyToken');
+  }
+  console.log('\tInitializing Vault and Transparent Proxy');
+  // If any other testnet, initialize for development purposes
+  const rewardsVaultEncodedInitialize = rewardsVaultImpl.interface.encodeFunctionData(
+    'initialize',
+    [woas, incentivesController]
+  );
+  const [admin] = await getEthersSigners();
+  await waitForTx(
+    await rewardsVaultProxy['initialize(address,address,bytes)'](
+      rewardsVaultImpl.address,
+      await admin.getAddress(),
+      rewardsVaultEncodedInitialize
+    )
+  );
 
-    const rewardsVaultImpl = await getPalmyRewardsVaultImpl();
-    const rewardsVaultProxy = await getPalmyRewardsVault();
-    const woas = await getWOASTokenPerNetwork(localBRE.network.name as eEthereumNetwork);
-    if (!rewardsVaultImpl.address || rewardsVaultImpl.address == ZERO_ADDRESS) {
-      throw new Error('Missing rewardsVaultImpl');
-    }
-    if (!rewardsVaultProxy.address || rewardsVaultImpl.address == ZERO_ADDRESS) {
-      throw new Error('Missing rewardsVaultProxy');
-    }
-    if (!woas || rewardsVaultImpl.address == ZERO_ADDRESS) {
-      throw new Error('Missing plmyToken');
-    }
-    console.log('\tInitializing Vault and Transparent Proxy');
-    // If any other testnet, initialize for development purposes
-    const rewardsVaultEncodedInitialize = rewardsVaultImpl.interface.encodeFunctionData(
-      'initialize',
-      [woas, incentivesController]
-    );
-    const [admin] = await getEthersSigners();
-    await waitForTx(
-      await rewardsVaultProxy['initialize(address,address,bytes)'](
-        rewardsVaultImpl.address,
-        await admin.getAddress(),
-        rewardsVaultEncodedInitialize
-      )
-    );
-
-    console.log('\tFinished Plmy Token and Transparent Proxy initialization');
-  });
+  console.log('\tFinished Plmy Token and Transparent Proxy initialization');
+});
